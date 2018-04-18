3b7aba7d6222e72629715d7d131ac6aa0cc9df9b
Bimal Paul <bimalbpaul@outlook.com>
2018-04-17 20:42:53 -0600

    Add business cards to local storage and retrieve and display

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 1.  Open your favorite terminal/command prompt and run `git clone git@github.com:clarkio/ng-ai-hack.git`
 2.  Register for the Azure Cognitive Services you plan to use
 3.  Retrieve the API key provided for each service
-4.  Update the `environment.example.ts` file by adding your key values in the `apiKeys` array
-5.  Rename the `environment.example.ts` file to `environment.ts`
+4.  Update the `environment.ts` file by adding your key values in the `apiKeys` array
+5.  Rename the `environment.ts` file to `environment.ts`
 6.  Go back to your terminal/command prompt and change to the directory of where you cloned this project. Example `cd ng-ai-hack`
 7.  Run `npm i`
 8.  Run `npm i -g @angular/cli`
@@ -26,8 +26,8 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 2.  Retrieve the API key provided for each service
 3.  Go to [this Stackblitz](https://stackblitz.com/github/clarkio/ng-ai-hack)
 4.  Click the "Fork" button
-5.  Update the `environment.example.ts` file by adding your key values in the `apiKeys` array
-6.  Rename the `environment.example.ts` file to `environment.ts`
+5.  Update the `environment.ts` file by adding your key values in the `apiKeys` array
+6.  Rename the `environment.ts` file to `environment.ts`
 
 ## References
 
--- src/app/demo/computer-vision/computer-vision-read-text/computer-vision-read-text.component.ts
+++ src/app/demo/computer-vision/computer-vision-read-text/computer-vision-read-text.component.ts
@@ -1,38 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { SafeResourceUrl, DomSanitizer, Title } from '@angular/platform-browser';
-import { VisionComponent } from '../../vision/vision.component';
-import { IOcrResult } from '../../../cognitive-services/models/vision.models';
-import { VisionDataService } from '../../../cognitive-services/vision-data.service';
-import { environment } from '../../../../environments/environment';
+import {Component, OnInit} from '@angular/core';
+import {SafeResourceUrl, DomSanitizer, Title} from '@angular/platform-browser';
+import {VisionComponent} from '../../vision/vision.component';
+import {IOcrResult} from '../../../cognitive-services/models/vision.models';
+import {VisionDataService} from '../../../cognitive-services/vision-data.service';
+import {environment} from '../../../../environments/environment';
+
+export interface BusinessCard {
+    url: string;
+    contents: string;
+}
 
 @Component({
     selector: 'app-computer-vision-read-text',
     templateUrl: './computer-vision-read-text.component.html',
     styleUrls: ['./computer-vision-read-text.component.css']
 })
+
 export class ComputerVisionReadTextComponent extends VisionComponent implements OnInit {
+    businessCards: BusinessCard[] = [];
     isLoading = true;
     errorMessage = '';
     showJSON = false;
     textResult: string;
     ocrResult: IOcrResult;
     apiTitle = 'Computer Vision API - Read Text';
-    apiDescription = 'Extract rich information from images to categorize and process visual data—and protect your users from unwanted content.';
+    apiDescription = '';
 
     constructor(protected sanitizer: DomSanitizer, private titleService: Title,
-      private visionDataService: VisionDataService) {
-      super(sanitizer);
-      this.titleService.setTitle('Read Text');
-  }
+                private visionDataService: VisionDataService) {
+        super(sanitizer);
+        this.titleService.setTitle('Read Text');
+    }
 
     ngOnInit() {
+        this.businessCards = JSON.parse(localStorage.getItem('cards'));
         this.imageList = environment.textImageUrls;
-        this.internetImageUrl = environment.textImageUrls[0];
+        // this.internetImageUrl = environment.textImageUrls[0];
         this.onInternetUrlSelected();
     }
 
     toggleJSON(b: boolean) {
         this.showJSON = b;
+    }
+
+    saveToLocal() {
+        this.businessCards.push({
+            url: this.selectedImageUrl.toString(),
+            contents: this.textResult
+        });
+        localStorage.setItem('cards', JSON.stringify(this.businessCards));
     }
 
     refreshDetection() {
@@ -84,6 +100,12 @@ export class ComputerVisionReadTextComponent extends VisionComponent implements 
         }
 
         this.textResult = plainText;
+        this.saveToLocal();
         this.ocrResult = result;
+    }
+
+    removeFromList(index: number) {
+        this.businessCards.splice(index, 1);
+        localStorage.setItem('cards', JSON.stringify(this.businessCards));
     }
 }
